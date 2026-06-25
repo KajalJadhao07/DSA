d9db9b1cb0da7c425cf5fe71e4854475acbf2f99
KajalJadhao07 <166523843+KajalJadhao07@users.noreply.github.com>
2026-06-25 09:46:54 +0530

    Time: 0 ms (100%), Space: 13.3 MB (28.89%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-
-    int check(int mid, vector<int>& arr, int k){
-        vector<int> missing;
-        int j = 0;
-        int i = 1;
-        while(i <= mid && j < arr.size()){
-            if(arr[j] == i){
-                j++;
-            }else{
-                missing.push_back(i);
-                if(missing.size() >= k){
-                    return missing[missing.size()-1];
-                }
-            }
-            i++;
-        }
-
-        while(i <= mid){
-            missing.push_back(i);
-            if(missing.size() >= k){
-                return missing[missing.size()-1];
-            }
-            i++;
-        }
-        return -1;
-    }
     int findKthPositive(vector<int>& arr, int k) {
-        int low = 1;
-        int high = 1000 + *max_element(arr.begin(), arr.end());
-        int ans = high;
+        int low = 0;
+        int high = arr.size()-1;
 
         while(low <= high){
             int mid = low + (high-low)/2;
-            int res = check(mid, arr, k);
+            int missing = arr[mid]-(mid+1);
 
-            if(res != -1){
-                ans = mid;
-                high = mid-1;
-            }else{
+            if(missing < k){
                 low = mid+1;
+            }else{
+                high = mid-1;
             }
         }
-        return ans;
+        return low + k;
     }
 };
