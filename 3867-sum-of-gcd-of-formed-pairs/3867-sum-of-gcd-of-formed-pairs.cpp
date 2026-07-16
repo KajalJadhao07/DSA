a78e3d1a782191c18e8a7116e9a16a14073757fc
KajalJadhao07 <166523843+KajalJadhao07@users.noreply.github.com>
2026-07-16 08:04:16 +0530

    Time: 67 ms (85.87%), Space: 148.7 MB (97.39%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -7,21 +7,20 @@ public:
         return get_gcd(num2, num1 % num2);
     }
     long long gcdSum(vector<int>& nums) {
-        vector<int>gcd(nums.size());
         int mx = nums[0];
 
         for(int i=0; i<nums.size(); i++){
             mx = max(mx, nums[i]);
-            gcd[i] = get_gcd(mx, nums[i]);
+            nums[i] = get_gcd(mx, nums[i]);
         }
 
-        sort(gcd.begin(), gcd.end());
+        sort(nums.begin(), nums.end());
 
         int l =0;
-        int r = gcd.size()-1;
+        int r = nums.size()-1;
         long long sum = 0;
         while(l < r){
-            sum += get_gcd(gcd[l], gcd[r]);
+            sum += get_gcd(nums[l], nums[r]);
             l++;
             r--;
         }
