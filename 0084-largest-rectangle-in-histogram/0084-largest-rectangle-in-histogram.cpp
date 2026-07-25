8cf07c1de48e2b446c06a6b11222f741fb13923f
KajalJadhao07 <166523843+KajalJadhao07@users.noreply.github.com>
2026-07-25 23:16:53 +0530

    Time: 4 ms (99.34%), Space: 81.3 MB (72.1%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-    vector<int> find_nse(vector<int>& nums){
-        stack<int> stk;
-        int n = nums.size();
-        vector<int> res(n);
-
-        for(int i = n-1; i>=0; i--){
-            while(!stk.empty() && nums[stk.top()]>=nums[i]){
-                stk.pop();
-            }
-            if(stk.empty()){
-                res[i] = n;
-            }else{
-                res[i] = stk.top();
-            }
-            stk.push(i);
-        }
-        return res;
-    }
-
-    vector<int> find_pse(vector<int>& nums){
-        stack<int> stk;
-        int n = nums.size();
-        vector<int> res(n);
-
-        for(int i = 0; i < n; i++){
-            while(!stk.empty() && nums[stk.top()]>nums[i]){
-                stk.pop();
-            }
-            if(stk.empty()){
-                res[i] = -1;
-            }else{
-                res[i] = stk.top();
-            }
-            stk.push(i);
-        }
-        return res;
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse = find_nse(heights);
-        vector<int> pse = find_pse(heights);
-        int area = 0;
-
-        for(int i =0; i<nse.size(); i++){
-            area = max(area, (nse[i]-pse[i]-1)* heights[i]);
+        stack<int> stk;
+        int max_area = 0;
+        int n = heights.size();
+        for(int i=0; i<n; i++){
+            while(!stk.empty() && heights[stk.top()] > heights[i]){
+                int element = stk.top();
+                stk.pop();
+                int nse = i;
+                int pse = stk.empty() ? -1 : stk.top();
+                max_area = max(max_area, (nse-pse-1)*heights[element]);
+            }
+            stk.push(i);
         }
-        return area;
+
+        while(!stk.empty()){
+            int nse = n;
+            int element = stk.top();
+            stk.pop();
+            int pse = stk.empty() ? -1 : stk.top();
+            max_area = max(max_area, (nse-pse-1)*heights[element]);
+        }
+        return max_area;
     }
 };
